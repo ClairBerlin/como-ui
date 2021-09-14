e53fda13f7ee5a3ad5801d64d31a516d31cb704d
Christian Rotzoll <ch.rotzoll@gmail.com>
2021-09-14 10:17:26 +0200

    chore: remove TODO from Profile Menu

--- src/components/ProfileMenu.vue
+++ src/components/ProfileMenu.vue
@@ -8,7 +8,6 @@ import PopoverMenu from "./PopoverMenu.vue";
 export default {
   components: { PopoverMenu },
   props: { title: String },
-  // TODO: get list of organizations and allow to switch directly from the context menu
   setup(props) {
     return {
       menu: { text: props.title },
