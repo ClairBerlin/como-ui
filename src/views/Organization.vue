be00836cfe387188938e40b5ca5f90951941b1a2
Ulrich Schuster <ulrich.schuster@koing.de>
2021-10-12 17:07:35 +0200

    feat (view): View to update an organization
    Enhance the Organization view with the possibility to update an organization's name and description.
    The view is not styled yet.
    Open question: Should we provide a separate read-only view?

--- src/views/Organization.vue
+++ src/views/Organization.vue
@@ -1,4 +1,81 @@
-<script setup></script>
+<script setup>
+import { onMounted, ref } from "vue";
+import { computed } from "@vue/reactivity";
+import { useStore } from "vuex";
+import { useRouter, useRoute } from "vue-router";
+import { roleToString } from "../utils/index";
+
+const store = useStore();
+const router = useRouter();
+const route = useRoute();
+
+const newOrgName = ref(undefined);
+const newOrgDescription = ref(undefined);
+
+onMounted(
+  async () =>
+    await store.dispatch("jv/get", `organizations/ ${route.params.id}`)
+);
+
+const currentOrg = computed(() =>
+  store.getters["jv/get"]({
+    _jv: { type: "Organization", id: route.params.id },
+  })
+);
+
+const orgMembership = computed(() =>
+  store.getters["authuser/getMembershipByOrgId"](route.params.id)
+);
+
+const orgRole = computed(() => roleToString(orgMembership.value.role));
+const userCanUpdate = computed(() => orgRole === "Owner");
+
+const updateOrganization = (event) => {
+  if (newOrgName.value || newOrgDescription.value) {
+    let updatedOrg = {
+      _jv: {
+        type: "Organization",
+        id: route.params.id,
+      },
+    };
+    if (newOrgName.value) {
+      updatedOrg["name"] = newOrgName.value;
+    }
+    if (newOrgDescription.value) {
+      updatedOrg["description"] = newOrgDescription.value;
+    }
+    console.log(updatedOrg);
+    store.dispatch("jv/patch", [
+      updatedOrg,
+      { url: `organizations/${route.params.id}/` },
+    ]);
+  }
+};
+</script>
+
 <template>
-  <div class="text-black">{{ $route.params.id }}</div>
+  <div class="ring-1 ring-gray-300 rounded-md bg-white">
+    <div class="ring-1 ring-gray-300 rounded-md bg-white">
+      <label>
+        <span>Organization Name</span>
+      </label>
+      <input
+        type="text"
+        v-model.trim="newOrgName"
+        :placeholder="currentOrg.name"
+      />
+    </div>
+    <div class="ring-1 ring-gray-300 rounded-md bg-white">
+      <label>
+        <span>Description</span>
+      </label>
+      <textarea
+        v-model.trim="newOrgDescription"
+        :placeholder="currentOrg.description"
+      ></textarea>
+    </div>
+    <button :disabled="!userCanUpdate" @click="updateOrganization">
+      Update
+    </button>
+  </div>
 </template>
