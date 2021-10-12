0b6d928752d9e74394569120e4bdc2d2dfe3a02d
Christian Rotzoll <ch.rotzoll@gmail.com>
2021-10-12 18:54:26 +0200

    feat: add list of members under /orgs/:id

---
+++ src/components/OptionsButton.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
+const props = defineProps({
+  icon: { type: Function, default: undefined },
+  buttonText: { type: String, default: undefined },
+  options: { type: Array, required: true },
+});
+</script>
+
+<template>
+  <Menu>
+    <MenuButton
+      ><component v-if="icon" :is="icon" class="h-5 w-5" />
+      <div v-else>{{ buttonText }}</div></MenuButton
+    >
+    <MenuItems>
+      <MenuItem v-for="option in options" :key="option.href">
+        <router-link :to="option.href">{{ option.title }}</router-link>
+      </MenuItem>
+    </MenuItems>
+  </Menu>
+</template>
--- src/views/Organization.vue
+++ src/views/Organization.vue
@@ -1,74 +1,60 @@
 <script setup>
 import { onMounted, ref } from "vue";
-import { computed } from "@vue/reactivity";
-import { useStore } from "vuex";
+import { CogIcon } from "@heroicons/vue/outline";
 import { useRoute } from "vue-router";
+import { useStore } from "vuex";
+import OptionsButton from "@/components/OptionsButton.vue";
 
-const store = useStore();
 const route = useRoute();
+const store = useStore();
+const orgId = route.params.id;
+const members = ref([]);
+const icon = CogIcon;
+const options = [
+  //TODO: adapt to correct urls/onclick actions
+  { href: "/change-role", title: "Change role" },
+  { href: "/remove", title: "Remove" },
+];
 
-const newOrgName = ref(undefined);
-const newOrgDescription = ref(undefined);
-
-onMounted(
-  async () => await store.dispatch("jv/get", `organizations/${route.params.id}`)
-);
-
-const currentOrg = computed(() =>
-  store.getters["jv/get"]({
-    _jv: { type: "Organization", id: route.params.id },
-  })
-);
-
-const orgMembership = computed(() =>
-  store.getters["authuser/getMembershipByOrgId"](route.params.id)
-);
-
-const isOwner = computed(() => orgMembership.value?.role === "O");
-
-const updateOrganization = () => {
-  if (newOrgName.value || newOrgDescription.value) {
-    let updatedOrg = {
-      _jv: {
-        type: "Organization",
-        id: route.params.id,
-      },
-    };
-    if (newOrgName.value) {
-      updatedOrg["name"] = newOrgName.value;
-    }
-    if (newOrgDescription.value) {
-      updatedOrg["description"] = newOrgDescription.value;
-    }
-    store.dispatch("jv/patch", [
-      updatedOrg,
-      { url: `organizations/${route.params.id}/` },
-    ]);
-  }
-};
+onMounted(async () => {
+  const membersIds = await store.dispatch(
+    "jv/get",
+    `organizations/${orgId}/users`
+  );
+  const getMembers = Object.keys(membersIds).map(async (id) => {
+    const member = await store.dispatch("jv/get", `users/${id}`);
+    // TODO: get role
+    members.value.push(member);
+  });
+  await Promise.all(getMembers);
+  console.log({ members });
+});
 </script>
 
 <template>
-  <div class="ring-1 ring-gray-300 rounded-md bg-white text-black">
-    <div class="ring-1 ring-gray-300 rounded-md bg-white">
-      <label>
-        <span>Organization Name</span>
-      </label>
-      <input
-        type="text"
-        v-model.trim="newOrgName"
-        :placeholder="currentOrg.name"
-      />
+  <div>
+    <div class="text-black">
+      <div class="flex justify-between">
+        <div class="">{{ $route.params.id }}</div>
+        <div class="gray-button">Invite Members (btn)</div>
+      </div>
+      <div class="ring-1 ring-gray-300 rounded-md bg-white text-lg">
+        <div
+          v-for="member in members"
+          :key="member.id"
+          class="flex justify-between border-b p-4"
+        >
+          <div>
+            <div class="flex">
+              {{ member.first_name }} {{ member.last_name }}
+            </div>
+            <div class="text-gray-700 text-sm">{{ member.username }}</div>
+          </div>
+          <div>{{ member.email }}</div>
+          <div>role</div>
+          <OptionsButton :options="options" :icon="icon" />
+        </div>
+      </div>
     </div>
-    <div class="ring-1 ring-gray-300 rounded-md bg-white">
-      <label>
-        <span>Description</span>
-      </label>
-      <textarea
-        v-model.trim="newOrgDescription"
-        :placeholder="currentOrg.description"
-      ></textarea>
-    </div>
-    <button v-if="isOwner" @click="updateOrganization">Update</button>
   </div>
 </template>
