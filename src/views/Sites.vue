922f9de28fb06e93114ea2890155cb8f48d5c741
Ulrich Schuster <ulrich.schuster@koing.de>
2021-11-03 11:01:39 +0100

    feat: Add summary views for sites, rooms, and sensors

--- src/views/Sites.vue
+++ src/views/Sites.vue
@@ -1,3 +1,41 @@
+<script setup>
+import { onMounted, ref, watch } from "vue";
+import { useRoute } from "vue-router";
+import { computed } from "@vue/reactivity";
+import { useStore } from "vuex";
+
+const route = useRoute();
+const store = useStore();
+
+// This view is routed to in an organization context only, this orgId is defined.
+const currentOrgId = computed(() => route.params.orgId);
+const sites = ref(undefined);
+
+const formatAdress = (address) => {
+  return `${address.street1}, ${address.zip} ${address.city}`;
+};
+
+const hasSites = computed(() => sites.value?.length > 0);
+
+async function update() {
+  const siteObj = await store.dispatch("jv/get", [
+    "sites",
+    { params: { "filter[organization]": currentOrgId.value } },
+  ]);
+  const siteList = Object.entries(siteObj);
+  console.log(`Organization has ${siteList.length} site(s).`);
+  sites.value = siteList.map(([_, site]) => site);
+  const relatedResourcePromises = siteList.map(([siteId, _]) => {
+    console.log(`Fetch related objects for site ${siteId}.`);
+    return store.dispatch("jv/getRelated", `sites/${siteId}`);
+  });
+  await Promise.all(relatedResourcePromises);
+}
+
+onMounted(async () => update());
+watch(currentOrgId, () => update());
+</script>
+
 <template>
   <div class="bg-gray-50">
     <div
@@ -24,7 +62,26 @@
       </h2>
     </div>
     <div>
-      This view will list all the sites of the selected organization, together with their adresses, and provide links to the rooms in each site. It will offer the possibility to add a new site and to remove an existing site with all its rooms and installations.
+      This view will list all the sites of the selected organization, together
+      with their adresses, and provide links to the rooms in each site. It will
+      offer the possibility to add a new site and to remove an existing site
+      with all its rooms and installations.
+    </div>
+    <div v-if="hasSites">
+      Sites:
+      <ul id="site-list">
+        <li v-for="site in sites" :key="site._jv.id">
+          ID: {{ site._jv.id }}, Name: {{ site.name }}, Address:
+          {{ formatAdress(site.address) }}
+          <router-link
+            :to="{
+              name: 'site',
+              params: { siteId: site._jv.id },
+            }"
+            >Inspect and modify site</router-link
+          >
+        </li>
+      </ul>
     </div>
   </div>
 </template>
