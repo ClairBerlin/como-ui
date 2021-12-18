fdbc565293e0547850b92ed52d1f4f4034f4581f
Christian Rotzoll <ch.rotzoll@gmail.com>
2021-12-18 21:21:06 +0100

    feat: add TextInput component

---
+++ src/components/forms/TextInput.vue
@@ -0,0 +1,59 @@
+<script setup>
+import { useField } from "vee-validate";
+
+const props = defineProps({
+  type: {
+    type: String,
+    default: "text",
+  },
+  value: {
+    type: String,
+    default: "",
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  label: {
+    type: String,
+    required: true,
+  },
+  successMessage: {
+    type: String,
+    default: "",
+  },
+  placeholder: {
+    type: String,
+    default: "",
+  },
+});
+const {
+  value: inputValue,
+  errorMessage,
+  handleBlur,
+  handleChange,
+  meta,
+} = useField(props.name, undefined, { initialValue: props.value });
+</script>
+
+<template>
+  <div
+    class="TextInput"
+    :class="{ 'has-error': !!errorMessage, success: meta.valid }"
+  >
+    <label :for="name">{{ label }}</label>
+    <input
+      :name="name"
+      :id="name"
+      :type="type"
+      :value="inputValue"
+      :placeholder="placeholder"
+      @input="handleChange"
+      @blur="handleBlur"
+    />
+
+    <p class="help-message" v-show="errorMessage || meta.valid">
+      {{ errorMessage || successMessage }}
+    </p>
+  </div>
+</template>
