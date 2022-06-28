f9e0230d8aeebf0bd79c59a490dca2f817e6100c
Urs Schmidt <urs.schmidt@ts.berlin>
2022-06-28 17:06:18 +0200

    InstallationViz styling update

--- src/components/CO2Graph/Tooltip.vue
+++ src/components/CO2Graph/Tooltip.vue
@@ -1,15 +1,15 @@
 <script setup>
-const props = defineProps({
+defineProps({
   time: { type: String, required: true },
   value: { type: String, required: true },
 });
-
-console.log("Props", props.time, props.value);
 </script>
 
 <template>
-  <div class="flex w-full items-center justify-between pl-10">
-    <span class="text-xs text-[#7886C2]">{{ time }} Uhr</span>
-    <span class="text-lg text-[#1E398F]">{{ value }} ppm</span>
+  <div class="flex h-7 w-full items-center justify-between pl-10">
+    <span v-if="time" class="text-xs text-[#7886C2]">{{ time }} Uhr</span>
+    <span v-if="value" class="text-lg font-bold text-[#1E398F]"
+      >{{ value }} ppm</span
+    >
   </div>
 </template>
