e92460e9b1842a5c9f41dbd4a6f0f0778cdb0169
Urs Schmidt <urs.schmidt@ts.berlin>
2022-06-29 14:59:53 +0200

    translate widget and format time after locale

--- src/components/InstallationViz/Tooltip.vue
+++ src/components/InstallationViz/Tooltip.vue
@@ -7,7 +7,7 @@ defineProps({
 
 <template>
   <div class="flex h-7 w-full items-center justify-between pl-10">
-    <span v-if="time" class="text-xs text-[#7886C2]">{{ time }} Uhr</span>
+    <span v-if="time" class="text-xs text-[#7886C2]">{{ time }}</span>
     <span v-if="value" class="text-lg font-bold text-[#1E398F]"
       >{{ value }} ppm</span
     >
